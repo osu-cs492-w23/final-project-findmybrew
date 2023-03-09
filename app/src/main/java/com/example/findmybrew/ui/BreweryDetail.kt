7e26cacc5ccbaf88b449de12457f6503f0df939f
hannacol <91215417+hannacol@users.noreply.github.com>
2023-03-09 14:05:26 -0800

    PR: Updated UI for BreweryDetail and Landing page (#12)
    * Added video on landing page
    
    * figured out jpg image scaling and colored jpg
    
    * added textviews for all api calls for brewdetail activity.
    
    * Removed white background from beerlogo image and changed font color to white
    
    ---------
    
    Co-authored-by: WackyWillard <WackyWillard@github.com>
    Co-authored-by: melcherj22 <melcherj@oregonstate.edu>

--- app/src/main/java/com/example/findmybrew/ui/BreweryDetail.kt
+++ app/src/main/java/com/example/findmybrew/ui/BreweryDetail.kt
@@ -5,11 +5,9 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.preference.PreferenceManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.findmybrew.R
-import com.example.findmybrew.data.PhotoOfBeer
 import com.example.findmybrew.data.SingleBrewery
 
 const val EXTRA_SINGLE_BREWERY = "com.example.android.findmybrew.SINGLE_BREWERY"
@@ -18,11 +16,11 @@ class BreweryDetail : AppCompatActivity() {
 
     private var brewery: SingleBrewery? = null
     private val viewModel: PhotosOfBeersViewModel by viewModels()
-    private val beerAdapter = BeerAdapter()
     private lateinit var photoOfBeersRV: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_brewery_detail)
+
 
         photoOfBeersRV = findViewById(R.id.rv_brewery_list)
         photoOfBeersRV.layoutManager = LinearLayoutManager(this)
@@ -35,14 +33,15 @@ class BreweryDetail : AppCompatActivity() {
             findViewById<TextView>(R.id.tv_brewery_name).text = brewery!!.name
 
 
+            findViewById<TextView>(R.id.tv_brewery_street).text = brewery!!.street
+            findViewById<TextView>(R.id.tv_brewery_city).text = brewery!!.city
+            findViewById<TextView>(R.id.tv_brewery_state).text = brewery!!.state
+            findViewById<TextView>(R.id.tv_brewery_postal).text = brewery!!.postal_code
+            findViewById<TextView>(R.id.tv_brewery_url).text = brewery!!.website_url
+
+
             viewModel.beer.observe(this) {
                 beerAdapter.updateBeer(it)
-
-                findViewById<TextView>(R.id.tv_brewery_street).text = brewery!!.street
-                findViewById<TextView>(R.id.tv_brewery_city).text = brewery!!.city
-                findViewById<TextView>(R.id.tv_brewery_state).text = brewery!!.state
-                findViewById<TextView>(R.id.tv_brewery_postal).text = brewery!!.postal_code
-                findViewById<TextView>(R.id.tv_brewery_url).text = brewery!!.website_url
             }
         }
     }
